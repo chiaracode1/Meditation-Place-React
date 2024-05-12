3b32a3b5458a3be1273b078c9e71a75bbd7832d2
chiaracode <138113937+chiaracode1@users.noreply.github.com>
2024-05-12 18:35:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: ''
-  },
-]);
-
-ReactDOM.render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <App />
+    </>,
+  );
